1bc35017966c42d88bb184eaae9983743e0fe169
icecold120000 <qej.pham@gmail.com>
2022-05-06 16:46:01 +0200

    accueil_connecte
    TypeEventFixture réglage

--- src/DataFixtures/TypeEventFixture.php
+++ src/DataFixtures/TypeEventFixture.php
@@ -13,21 +13,22 @@ class TypeEventFixture extends Fixture
 
         $types = [
             ['name' => "Assemblée territoriale", 'isObligated' => true],
-            ["Conseil territorial", 'isObligated' => false],
-            ["Conseil territorial élargi", 'isObligated' => true],
-            ["Réunion équipe territoriale", 'isObligated' => false],
-            ["Réunion pôle pédagogique", 'isObligated' => false],
-            ["Réunion pôle adm. et financier", 'isObligated' => false],
-            ["Week-end territorial", 'isObligated' => true],
-            ["Conseil de groupe", 'isObligated' => true],
-            ["Réunion d'unité", 'isObligated' => true],
-            ["Conseil de chefs", 'isObligated' => false],
-            ["Week-end d'unité", 'isObligated' => true],
-            ["Réunion de groupe", 'isObligated' => false],
-            ["Week-end de groupe", 'isObligated' => false],
-            ["Réunion diverse", 'isObligated' => false]
+            ['name' => "Conseil territorial", 'isObligated' => false],
+            ['name' => "Conseil territorial élargi", 'isObligated' => true],
+            ['name' => "Réunion équipe territoriale", 'isObligated' => false],
+            ['name' => "Réunion pôle pédagogique", 'isObligated' => false],
+            ['name' => "Réunion pôle adm. et financier", 'isObligated' => false],
+            ['name' => "Week-end territorial", 'isObligated' => true],
+            ['name' => "Conseil de groupe", 'isObligated' => true],
+            ['name' => "Réunion d'unité", 'isObligated' => true],
+            ['name' => "Conseil de chefs", 'isObligated' => false],
+            ['name' => "Week-end d'unité", 'isObligated' => true],
+            ['name' => "Réunion de groupe", 'isObligated' => false],
+            ['name' => "Week-end de groupe", 'isObligated' => false],
+            ['name' => "Réunion diverse", 'isObligated' => false]
         ];
 
+        $i = 0;
         foreach ($types as $type) {
             $typeEvent1 = new TypeEvent();
             $typeEvent1
@@ -35,7 +36,7 @@ class TypeEventFixture extends Fixture
                 ->setIsObligated($type['isObligated']);
 
             $manager->persist($typeEvent1);
-            $this->addReference('type_event0', $typeEvent1);
+            $this->addReference('type_event_' . $i++, $typeEvent1);
         }
 
         $manager->flush();
